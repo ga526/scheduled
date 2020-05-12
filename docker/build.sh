9e2360c50b51353f817d68f482c54f5743abdb1e
pc <pc>
2020-05-12 10:15:19 +0800

    完善shell脚本

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+nowday=$(date "+%Y%m%d%H%M");
+rm -rf docker/step_*.txt;
 echo '1'>docker/step_1.txt && go build main.go;
 echo '2'>docker/step_2.txt && chmod -R 777 main;
-echo '3'>docker/step_3.txt && ./main &;
+echo '3'>docker/step_3.txt && ./main >>docker/run_log_${nowday}.txt 2>&1 ;
+echo '4'>docker/step_success.txt;
