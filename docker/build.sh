ff23e39468a67db674a97d523255f969569721f8
pc <pc>
2020-05-12 09:36:42 +0800

    完善docker脚本

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-cd /go/src && go build main.go && chmod -R 777 main && ./main
+echo '1'>docker/step_1.txt && go build main.go;
+echo '2'>docker/step_2.txt && chmod -R 777 main;
+echo '3'>docker/step_3.txt && ./main &;
